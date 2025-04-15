bbf46f32a1a97197baa62892f1e165f2f891c522
Nehrusai4 <nehrusai4@gmail.com>
2025-04-16 03:12:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll for nav links
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const section = document.querySelector(link.getAttribute('href'));
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+});
